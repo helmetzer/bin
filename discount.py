31eb83efa4b8f48be0bec850f8aaf404aac7f5d4
Horst Fickenscher <helmetzer@web.de>
2021-09-03 09:21:58 +0200

    m2202

--- discount.py
+++ discount.py
@@ -5,17 +5,17 @@ KMAX = 5000
 
 # 2022 02
 angelegt = [
-        14036,
-        14478,
+        6840 + 6200,
         14824,
         14500,
-        108*125
+        108*125,
+        108*125,
         ]
 
-frei = 19554 - 2500 - 800
+frei = 18600
 
 cap = 125
-preis = 123.58
+preis = 123.84
 
 # print('My first python script')
 
@@ -24,7 +24,9 @@ s = sum(angelegt)
 print('angelegt', s)
 print('frei', frei)
 
-ziel = round((frei + s - KMAX)/(len(angelegt) + 1))
+la = len(angelegt)
+assert la == 5, "angelegt falsche länge"
+ziel = round((frei + s - KMAX)/(la + 1))
 stueck = round(ziel/cap)
 print('Ziel', ziel)
 print('Stueck', stueck)
