f60d5b00842aba9105bc397a1fab69d123ead9ba
Horst Fickenscher <helmetzer@web.de>
2021-12-17 14:39:03 +0100

    Keine .* Dateien, aber .thunderbird

--- save.sh
+++ save.sh
@@ -4,13 +4,15 @@
 
 TAG=CACHEDIR.TAG
 # HOST=$(hostname)
-STICK=/media/horst/INTENSO/Sicherung
-cd $STICK || exit 1
+STICK=/media/$USER/INTENSO/Sicherung
+cd $STICK || { echo Stick nicht oder falsch eingehaengt ; exit 1 ; }
 DIR=$USER-$(date +%g%m%d%H%M)
-# mkdir $DIR  || exit 1
-(cd /home ; tar -Oc --exclude-tag $TAG $USER) | tar -xvf -
-mv $USER $DIR
-ls
+mkdir $DIR  || exit 1
+cd $DIR || exit 1
+# save everything but .* - save .thunderbird
+(cd $HOME ; tar -Oc --exclude-tag $TAG * .thunderbird) | tar -xvf -
+ls ..
+
 df
 
 exit
