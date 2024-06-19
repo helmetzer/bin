d02493993b42f8b347fd0e65b7aea517def9d7c0
Horst Fickenscher <helmetzer@web.de>
2024-06-19 14:15:35 +0200

    foo

--- cashneu.sh
+++ cashneu.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-echo Achtung Jahr anpassen.
-exit
 
-sed s/2021-/2022-/g Temp.gnucash > 2022.gnucash
+
+
+cd $HOME/.local/share/gnucash/books
+# cp Temp.gnucash.gcm 2024.gnucash.gcm
+# exit
+cd /daten/Users/Horst/Dokumente/GnuCash  || exit 1
+sed s/2023-/2024-/g Temp.gnucash > 2024.gnucash
+exit
+# cp $JAHR.gnucash Temp.gnucash
+
--- steuer.py
+++ steuer.py
@@ -1,51 +1,62 @@
 #   module steuer
 
+jahr = 2024
 # 2023
-grund = 10908
-next = 15999
-next2 = 62809
-next3 = 277825
-f1 = 979.18
-f2 = 192.59
-s1 = 966.53
-s2 = 9972.98
-s3 = 18307.73
+if jahr == 2023:
+    grund = 10908
+    next = 15999
+    next2 = 62809
+    next3 = 277825
+    f1 = 979.18
+    f2 = 192.59
+    s1 = 966.53
+    s2 = 9972.98
+    s3 = 18307.73
+else:
 
-# 2024
-grund = 11604
-next = 17005
-next2 = 66760
-next3 = 277825
-f1 = 922.98
-f2 = 181.9
-s1 = 1025.38
-s2 = 10602.13
-s3 = 18936.88
+    # 2024
+    grund = 11604
+    next = 17005
+    next2 = 66760
+    next3 = 277825
+    f1 = 922.98
+    f2 = 181.9
+    s1 = 1025.38
+    s2 = 10602.13
+    s3 = 18936.88
 
 ag = 0.2451
+k1 = 0.08
+k2 = 1 + k1
+soli = 0.055
+solik = 1.0 + soli + k1
+
 
 def ags(von):
 
-    return 1.135*ag*von
+    return solik*ag*von
 
 def kags(von):
 
-    return 0.08*ag*von
+    return k1*ag*von
 
 def steuer(von):
 
     if von <= grund:
-        return 0
+        st = 0
 
-    if von <= next:
+    elif von <= next:
         y = (von - grund)/10000
-        return (f1*y + 1400)*y
+        st = (f1*y + 1400)*y
 
-    if von <= next2:
+    elif von <= next2:
         y = (von - next)/10000
-        return (f2*y + 2397)*y + s1
+        st = (f2*y + 2397)*y + s1
 
-    if von <= next3:
-        return 0.42*von - s2
+    elif von <= next3:
+        st = 0.42*von - s2
 
-    return 0.45*von - s3
+    else:
+        st = 0.45*von - s3
+
+    return st*k2
