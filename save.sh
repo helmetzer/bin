690290067a1ad13308f0b15a05f1b9315a8d22f3
Horst Fickenscher <helmetzer@web.de>
2024-06-19 14:03:14 +0200

    Sichern von /daten/Users/$USER

--- save.sh
+++ save.sh
@@ -10,7 +10,9 @@ DIR=$USER-$(date +%g%m%d%H%M)
 mkdir $DIR  || exit 1
 cd $DIR || exit 1
 # save everything but .* - save .thunderbird
-(cd $HOME ; tar -Oc --exclude-tag $TAG * .thunderbird) | tar -xvf -
+## (cd $HOME ; tar -Oc --exclude-tag $TAG * .thunderbird) | tar -xvf -
+(cd /daten/Users/$USER ; tar -Oc --exclude-tag $TAG *) | tar -xvf -
+
 ls ..
 
 df
